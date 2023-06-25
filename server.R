c97acdf75a6bc80cc94204758ebc1c524f1a4c3e
Joel Seah <131426087+Wizardomahousukai@users.noreply.github.com>
2023-06-25 21:18:32 +0800

    Add files via upload

---
+++ server.R
@@ -0,0 +1,23 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+install.packages("ngram")
+library(ngram)
+
+library(shiny)
+
+# Define server logic required to draw a histogram
+function(input, output, session) {
+
+    output$distPlot <- renderText({
+      
+      predict(input$textInput)
+      
+    })
+
+}
