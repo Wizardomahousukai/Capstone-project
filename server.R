43380d7f88cc987c0f665f4d746b1052b908d8e1
Joel Seah <131426087+Wizardomahousukai@users.noreply.github.com>
2023-06-27 16:36:43 +0800

    Update server.R

--- server.R
+++ server.R
@@ -1,23 +1,25 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-install.packages("ngram")
-library(ngram)
-
 library(shiny)
+
+install.packages("sbo")
+library(sbo)
+
+head(sbo::twitter_train, 3)
+
+word <- sbo_predictor(object = sbo::twitter_train, # preloaded example dataset
+                      N = 3, # Train a 3-gram model
+                      dict = target ~ 0.75, # cover 75% of training corpus
+                      .preprocess = sbo::preprocess, # Preprocessing transformation 
+                      EOS = ".?!:;", # End-Of-Sentence tokens
+                      lambda = 0.4, # Back-off penalization in SBO algorithm
+                      L = 3L, # Number of predictions for input
+                      filtered = "<UNK>" # Exclude the <UNK> token from predictions
+)
+
+predictedword <- predict(word,"i love")
 
 # Define server logic required to draw a histogram
 function(input, output, session) {
 
-    output$distPlot <- renderText({
-      
-      predict(input$textInput)
-      
-    })
+    output$distPlot <- predictedword
 
 }
