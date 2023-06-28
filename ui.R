52ed88f070233aa35e488b56a69aa781049586d2
Joel Seah <131426087+Wizardomahousukai@users.noreply.github.com>
2023-06-28 16:16:21 +0800

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,17 +1,8 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 # load shiny package
 library(shiny)
 # begin shiny UI
 shinyUI(pageWithSidebar(
-  headerPanel("Smart prediction technology for easier mobile typing by Joel Seah"),
+  headerPanel("Smart prediction by Joel Seah"),
   sidebarPanel(
     textInput(inputId="text1", label = "Please enter some words"),
   ),
