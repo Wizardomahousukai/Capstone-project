032f706864e980499b37d488721614a6eb62d28b
Joel Seah <131426087+Wizardomahousukai@users.noreply.github.com>
2023-06-30 15:52:02 +0800

    Update Data Science capstone project final.R

--- Data Science capstone project final.R
+++ Data Science capstone project final.R
@@ -1,8 +1,7 @@
-
-install.packages("sbo")
 library(sbo)
+head(sbo::twitter_train, 3)
 
-word <- sbo_predictor(object = sbo::twitter_train, # preloaded example dataset
+p <- sbo_predictor(object = sbo::twitter_train, # preloaded example dataset
                    N = 3, # Train a 3-gram model
                    dict = target ~ 0.75, # cover 75% of training corpus
                    .preprocess = sbo::preprocess, # Preprocessing transformation 
@@ -12,4 +11,28 @@ word <- sbo_predictor(object = sbo::twitter_train, # preloaded example dataset
                    filtered = "<UNK>" # Exclude the <UNK> token from predictions
 )
 
-predictedword <- predict(word,"i love")
+predict(p, "i love")
+
+set.seed(840)
+babble(p)
+
+babble(p)
+
+babble(p)
+
+t <- sbo_predtable(object = sbo::twitter_train, # preloaded example dataset
+                   N = 3, # Train a 3-gram model
+                   dict = target ~ 0.75, # cover 75% of training corpus
+                   .preprocess = sbo::preprocess, # Preprocessing transformation 
+                   EOS = ".?!:;", # End-Of-Sentence tokens
+                   lambda = 0.4, # Back-off penalization in SBO algorithm
+                   L = 3L, # Number of predictions for input
+                   filtered = "<UNK>" # Exclude the <UNK> token from predictions
+)
+
+
+p <- sbo_predictor(t) # This is the same as 'p' created above
+
+save(t)
+# ... and, in another session:
+load("t.rda")
