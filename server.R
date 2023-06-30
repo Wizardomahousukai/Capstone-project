200b42d75ccdf5919e20c5b291b0176b39fa673d
Joel Seah <131426087+Wizardomahousukai@users.noreply.github.com>
2023-06-30 18:08:28 +0800

    Update server.R

--- server.R
+++ server.R
@@ -1,27 +1,62 @@
 library(shiny)
-
-source("C:/Users/Dell/Desktop/SBO.R")
-
-load("t.rda")
-
-t <- sbo_predtable(object = t.rda, # preloaded example dataset
-                   N = 3, # Train a 3-gram model
-                   dict = target ~ 0.75, # cover 75% of training corpus
-                   .preprocess = sbo::preprocess, # Preprocessing transformation 
-                   EOS = ".?!:;", # End-Of-Sentence tokens
-                   lambda = 0.4, # Back-off penalization in SBO algorithm
-                   L = 3L, # Number of predictions for input
-                   filtered = "<UNK>" # Exclude the <UNK> token from predictions
-)
-
-p <- sbo_predictor(t)
+library(shiny)
+library(tm)
+library(data.table)
 
 
-shinyServer(
-  function(input, output) {
-    output$top1 = renderPrint({
-      isolate(p(input$text1))
-    })
-    
+#load the data
+load("C:/Users/Dell/Desktop/Data/projectdata.Rdata")
+
+#function for predicting the next word
+#it searches for matching n-grams and
+#returns the top suggestions ranked by frequency
+word_predict <- function(sentence, n = 12){
+  
+  #clean the text
+  sentence <- tolower(removePunctuation(removeNumbers(sentence)))
+  
+  #splits the cleaned sentence into individual words and stores them in the words variable
+  words <- unlist(strsplit(sentence, split = " "))
+  
+  #selects the last five words from the words variable
+  words <- tail(words,2)
+  
+  word1 <- words[1]
+  word2 <- words[2]
+
+  df <- data.table()
+  
+  
+  if (nrow(df)==0 & !is.na(word2)) {
+    if(nrow(df) == 0) df <- subset(ngram3, w1==word1 & w2==word2)
+    if(nrow(df) == 0) df <- subset(ngram2, w1==word2)
   }
-)
+  
+  if (nrow(df)==0 & !is.na(word1)) {
+    if(nrow(df) == 0) df <- subset(ngram2, w1==word1)
+    if(nrow(df) == 0) df <- head(ngram1)
+  }
+  
+  if(nrow(df) > 0){
+    df$freq <- df$count / sum(df$count)
+    output <- as.data.frame(head(df[order(-freq)], min(n, nrow(df))))
+    output$count <- NULL
+    output$freq <- NULL
+    colnames(output) <- NULL
+    output
+  }
+  
+}
+
+#logic of the server:
+shinyServer(function(input, output) {
+  
+  # Predict next words
+  output$prediction <- renderPrint({
+    sentence <- input$sentence
+    n <- input$n
+    
+    word_predict(sentence, n)
+  })
+  
+})
