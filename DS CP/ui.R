d50ab3efa06c59243e44ac2180aa33c24bd7794e
Joel Seah <131426087+Wizardomahousukai@users.noreply.github.com>
2023-07-03 09:41:30 +0800

    Update ui.R

--- DS CP/ui.R
+++ DS CP/ui.R
@@ -2,10 +2,10 @@ library(shiny)
 
 # Define UI for application
 ui <- fluidPage(
-  titlePanel('Type a word below prediction by Joel'),
+  titlePanel('Type a word below and Joel will tell you the next word'),
   sidebarLayout(
     sidebarPanel(
-      textInput('sentence', 'What will your sentence be'),
+      textInput('sentence', 'your next word is'),
     ),
     mainPanel(
       h4('The most predicted words are'),
