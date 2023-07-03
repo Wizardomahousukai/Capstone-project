fa6d0c54b9b2788086607913dcd68883a8ef2ac5
Joel Seah <131426087+Wizardomahousukai@users.noreply.github.com>
2023-07-03 09:20:11 +0800

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,17 +1,1 @@
-# load shiny package
-library(shiny)
-# begin shiny UI
-shinyUI(pageWithSidebar(
-  headerPanel("Smart prediction by Joel Seah"),
-  sidebarPanel(
-    textInput(inputId="text1", label = "Please enter some words"),
-  ),
-  mainPanel(
-    h3('Prediction result'),
-    h4('Show the top n most likely words'),
-    verbatimTextOutput("topn"),
-    h4('Prediction result of the next word'),
-    verbatimTextOutput("top1")
-    
-  )
-))
+
